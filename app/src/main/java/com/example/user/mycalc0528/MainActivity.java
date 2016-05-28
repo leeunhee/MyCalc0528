a8d1147944e76b1ffa8e3c3e735125a88e96480a
leeunhee <leeunhee@hanmail.net>
2016-05-28 18:12:01 +0900

    Start

--- app/src/main/java/com/example/user/mycalc0528/MainActivity.java
+++ app/src/main/java/com/example/user/mycalc0528/MainActivity.java
@@ -19,8 +19,8 @@ public class MainActivity extends Activity {
     Button btPlus, btMinus, btMuti, btDevide, btMod;
     EditText etNum1, etNum2;
     TextView Resultvalue1;
-    String resS1, resS2;
-    Integer res1;
+    String resS1;
+    int num1, num2, res1, res2, res3, res4, res5;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -28,8 +28,10 @@ public class MainActivity extends Activity {
         this.setTitle("간단계산 EHLEE");
         etNum1 = (EditText) findViewById(R.id.Input1);
         etNum2 = (EditText) findViewById(R.id.Input2);
-        resS1 = etNum1.getText().toString();
-        resS2 = etNum2.getText().toString();
+//        resS1 = etNum1.getText().toString();
+//        resS2 = etNum2.getText().toString();
+        num1 = Integer.parseInt(etNum1.toString());
+        num2 = Integer.parseInt(etNum2.toString());
 
         btPlus = (Button) findViewById(R.id.btPlus);
         btMinus = (Button) findViewById(R.id.btMinus);
@@ -38,12 +40,18 @@ public class MainActivity extends Activity {
         btMod = (Button) findViewById(R.id.btMod);
         Resultvalue1 = (TextView) findViewById(R.id.resultValue);
 
-        res1 = Integer.parseInt(resS1)+Integer.parseInt(resS2);
+//        res1 = Integer.parseInt(etNum1.toString())+Integer.parseInt(etNum2.toString());
+
+        res1 = num1+num2;
+        res2 = num1-num2;
+        res3 = num1*num2;
+        res4 = num1/num2;
+        res5 = num1%num2;
 
         btPlus.setOnTouchListener(new View.OnTouchListener() {
             @Override
             public boolean onTouch(View v, MotionEvent event) {
-                Resultvalue1.setText("계산결과 >> " + res1.toString());
+                Resultvalue1.setText("계산결과 >> " + res1);
                 return false;
             }
         });
